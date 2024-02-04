d8148ce68c22d06d0dab64a35877f1f77a2308a4
Mohammed Ali <mohammedaalli088@gmail.com>
2024-02-04 17:26:55 +0000

    feat: models field add up

--- models/student.py
+++ models/student.py
@@ -5,3 +5,4 @@ class Student(BaseModel):
     student_name: str
     student_email: str
     student_phone: str
+    student_program: str
--- schemas/student.py
+++ schemas/student.py
@@ -4,7 +4,8 @@ def studentEntity(db_item) -> dict:
         "id": str(db_item["_id"]),
         "name": db_item.get("student_name", "N/A"),
         "email": db_item.get("student_email", "N/A"),
-        "phone": db_item.get("student_phone", "N/A")
+        "phone": db_item.get("student_phone", "N/A"),
+        "program": db_item.get("student_program","N/A")
     }
 
 

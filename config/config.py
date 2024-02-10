d983394a01e9d4c1452605934ff07448e4f37e3c
Mohammed Ali <mohammedaalli088@gmail.com>
2024-02-10 05:43:59 +0000

    feat: mongoDB atlas setup

--- config/config.py
+++ config/config.py
@@ -1,4 +1,22 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from urllib.parse import quote_plus
+from pymongo import MongoClient
 
+# Define your MongoDB URI with the correct username and escaped password
+username = 'pundit'
+password = 'pundit@2035P#'
+escaped_password = quote_plus(password)
+uri = f"mongodb+srv://{username}:{escaped_password}@atlascluster.nep04rx.mongodb.net/?retryWrites=true&w=majority"
 
+client = MongoClient(uri)
+
+db = client.myBlogs
+
+blogsCollection = db['my blogs']
+
+# Create a new client and connect to the server
+try:
+    client = MongoClient(uri)
+    client.admin.command('ping')  # Send a ping to confirm a successful connection
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
